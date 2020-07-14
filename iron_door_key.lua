da6285d047f9547ef897e37925a6fd27efa18d96
Aidan Stewart <aidanstewart@sunsetdeveloper.com>
2020-07-13 22:44:57 -0700

    Update iron_door_key.lua

--- iron_door_key.lua
+++ iron_door_key.lua
@@ -1,6 +1,31 @@
 local args = {...}
 local lock_port = 20319
+local port = 19273
 local modem = peripheral.find("modem")
-os.loadAPI("aeslua")
-modem.transmit(lock_port, 10923, aeslua.encrypt(args[1], args[1]))
-term.clear()
+os.loadAPI("strutils")
+-- wget https://raw.githubusercontent.com/Xtansia/Lua-String-Utils-API/master/StrUtilsAPI.lua strutils
+ 
+ 
+function request_iron_door()
+    modem.transmit(lock_port, port, settings.get("password"))
+end
+ 
+ 
+function save_key()
+    modem.open(port)
+    settings.set("lock_id", args[1])
+    modem.transmit(lock_port, port, args[1])
+    print("Waiting for response from Iron Door server. If this message remains after some time, terminate the program.")
+    event, side, frequency, replyFrequency, message, distance = os.pullEvent("modem_message")  
+    term.clear()  
+    settings.set("password", strutils.SHA1(message .. args[2]))
+    modem.close(port)
+end
+ 
+ 
+if args[1] == nil then
+    request_iron_door()
+else
+    save_key()
+    request_iron_door()
+end
